aac5e8c56f23da19fdd321127551fa4d1bf9c7bc
matcalvani <matstraty@gmail.com>
2022-05-04 17:50:27 -0700

    mu-plugin

--- mu-plugins/post-types.php
+++ mu-plugins/post-types.php
@@ -1,10 +1,27 @@
 <?php
 
-// Register new post type workouts
+
 function post_types() {
+
+  // Post type events
+  register_post_type('event', array(
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Events',
+      'add_new_item' => 'Add New Event',
+      'edit_item' => 'Edit Event',
+      'all_items' => 'All Events',
+      'singular_name' => 'Event'
+    ),
+    'menu_icon' => 'dashicons-calendar'
+  ));
+
+  // Post type workouts
   register_post_type('workout', array(
     'has_archive' => true,
     'public' => true,
+    'show_in_rest' => true,
     'labels' => array(
       'name' => 'Workouts',
       'add_new_item' => 'Add New Workout',
@@ -15,34 +32,35 @@ function post_types() {
     'menu_icon' => 'dashicons-calendar-alt'
   ));
 
-  // Register new post type nutrition
-    register_post_type('nutrition', array(
-      'has_archive' => true,
-      'public' => true,
-      'labels' => array(
-        'name' => 'Nutrition',
-        'add_new_item' => 'Add New Nutrition',
-        'edit_item' => 'Edit Nutrition',
-        'all_items' => 'All Nutritions',
-        'singular_name' => 'Nutrition'
-      ),
-      'menu_icon' => 'dashicons-carrot'
-    ));
+  // Post type nutrition
+  register_post_type('nutrition', array(
+    'has_archive' => true,
+    'public' => true,
+    'show_in_rest' => true,
+    'labels' => array(
+      'name' => 'Nutrition',
+      'add_new_item' => 'Add New Nutrition',
+      'edit_item' => 'Edit Nutrition',
+      'all_items' => 'All Nutritions',
+      'singular_name' => 'Nutrition'
+    ),
+    'menu_icon' => 'dashicons-carrot'
+  ));
 
-    // Register new post type note
-    register_post_type('note', array(
-      'show_in_rest' => true,
-      'support' => array('title', 'editor'),
-      'public' => false,
-      'labels' => array(
-        'name' => 'Notes',
-        'add_new_item' => 'Add New note',
-        'edit_item' => 'Edit note',
-        'all_items' => 'All notes',
-        'singular_name' => 'note'
-      ),
-      'menu_icon' => 'dashicons-edit'
-    ));
+  // Post type note
+  register_post_type('note', array(
+    'show_in_rest' => true,
+    'support' => array('title', 'editor'),
+    'public' => false,
+    'labels' => array(
+      'name' => 'Notes',
+      'add_new_item' => 'Add New note',
+      'edit_item' => 'Edit note',
+      'all_items' => 'All notes',
+      'singular_name' => 'note'
+    ),
+    'menu_icon' => 'dashicons-edit'
+  ));
 }
 
 add_action('init', 'post_types');
