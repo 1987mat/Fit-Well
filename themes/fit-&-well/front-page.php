043c57759ea8fe1ef8c70fd2fbd7d2b2a0f1cf5c
matcalvani <matstraty@gmail.com>
2022-05-11 13:18:44 -0700

    compress img

--- themes/fit-&-well/front-page.php
+++ themes/fit-&-well/front-page.php
@@ -14,51 +14,37 @@
     <h2>Our Classes</h2>
     <div class="classes-container">
       <?php 
-          // Create custom query for Nutrition posts
+          // Create custom query for Class posts
           $homepageClasses = new WP_Query(array(
             'post_type' => 'class',
           ));
 
-          // Show event posts in the front page
+          // Show class posts in the front page
           while($homepageClasses->have_posts()) {
             $homepageClasses->the_post(); 
-            if(get_the_title() === 'Yoga') {
-              ?> 
-              <div class="single-class yoga">
-                <h3><?php the_title(); ?></h3>
-              </div>
-            <?php 
-            } else if (get_the_title() == 'Zumba') {
-              ?>
-                <div class="single-class zumba">
-                  <h3><?php the_title(); ?></h3>
-                </div>
-              <?php 
-            } else if (get_the_title() == 'Outdoor'){
-            ?> 
-            <div class="single-class outdoor">
+            $classTitle = lcfirst(get_the_title());
+            ?>
+
+            <!-- Have different classes based on post title -->
+            <div class="single-class <?php echo $classTitle; ?>">
               <h3><?php the_title(); ?></h3>
-            </div>
-            <?php 
-            } else {
-              ?> 
-              <div class="single-class cycling">
-                <h3><?php the_title(); ?></h3>
-              </div>
-            <?php 
-            }
-          } 
+            </div>  
+
+          <?php } 
           wp_reset_postdata();
-        ?>
+          ?>
     </div>
   </section>
 
-  <section class="our-history">
-    <?php
-      $img_src = get_template_directory_uri() . '/images/history.jpg';
-    ?>
-    <img src="<?php echo $img_src ?>">
-    <p>Text here</p>
+  <section class="history-section">
+    <h2>Our History</h2>
+    <div class="our-history">
+      <?php
+        $img_src = get_template_directory_uri() . '/images/history-img.jpg';
+      ?>
+      <img src="<?php echo $img_src ?>">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
   </section>
 
   <section class="show-posts">
